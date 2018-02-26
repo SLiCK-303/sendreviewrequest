8a01e5c4d6db3b9db4c04e8354e9e6f7057de637
Mark <slick_303@hotmail.com>
2018-02-25 18:17:19 -0600

    Updated copyright info

--- sendreviewrequest.php
+++ sendreviewrequest.php
@@ -1,22 +1,19 @@
 <?php
 /**
- * Copyright (C) 2018 thirty bees
+ * Copyright (C) 2018 SLiCK-303
  *
  * NOTICE OF LICENSE
  *
  * This source file is subject to the Academic Free License (AFL 3.0)
- * that is bundled with this package in the file LICENSE.txt.
+ * that is bundled with this package in the file LICENSE.md.
  * It is also available through the world-wide-web at this URL:
  * http://opensource.org/licenses/afl-3.0.php
- * If you did not receive a copy of the license and are unable to
- * obtain it through the world-wide-web, please send an email
- * to license@thirtybees.com so we can send you a copy immediately.
  *
- * @package   sendreviewrequest
- * @author    slick-303 <slick_303@hotmail.com>
- * @copyright 2017-2018 SLiCK-303
- * @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
- */
+ * @package    sendreviewrequest
+ * @author     SLiCK-303 <slick_303@hotmail.com>
+ * @copyright  2018 SLiCK-303
+ * @license    http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+**/
 
 if (!defined('_TB_VERSION_')) {
 	exit;
