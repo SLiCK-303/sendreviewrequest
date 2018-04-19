9550e9a57b71fc2b29ea5c52c3368cf53f9525e9
Mark <slick_303@hotmail.com>
2018-04-18 19:51:41 -0500

    Updated German BO

--- translations/de.php
+++ translations/de.php
@@ -9,7 +9,7 @@ $_MODULE['<{sendreviewrequest}prestashop>sendreviewrequest_462390017ab0938911d2d
 $_MODULE['<{sendreviewrequest}prestashop>sendreviewrequest_003b06dcfe67596f17e3de26e6a436c8'] = 'Beim Aktualisieren der Einstellungen ist ein Fehler aufgetreten';
 $_MODULE['<{sendreviewrequest}prestashop>sendreviewrequest_0b3fcef1097d40c2e85bec749c7b8c0c'] = 'Klicken Sie, um zur Produktseite zu gelangen';
 $_MODULE['<{sendreviewrequest}prestashop>sendreviewrequest_fba6524bb4f34a4c7d8e84b082fc4ce1'] = 'Definieren Sie die Einstellungen und fügen Sie die folgende URL in Ihren Crontab ein oder rufen Sie diese täglich manuell auf:';
-$_MODULE['<{sendreviewrequest}prestashop>sendreviewrequest_a82be0f551b8708bc08eb33cd9ded0cf'] = 'Information';
+$_MODULE['<{sendreviewrequest}prestashop>sendreviewrequest_8d2ed33eda0b9f783929195f43412c41'] = 'Cron Information';
 $_MODULE['<{sendreviewrequest}prestashop>sendreviewrequest_0010ee7e087d7adb096eebfd2b919ffa'] = 'Bestellstatus';
 $_MODULE['<{sendreviewrequest}prestashop>sendreviewrequest_4f38921f9b29261d73d745d1308f3478'] = 'Bestellungen müssen den Status haben, damit eine E-Mail gesendet wird';
 $_MODULE['<{sendreviewrequest}prestashop>sendreviewrequest_498f79c4c5bbde77f1bceb6c86fd0f6d'] = 'Zeigen';
@@ -31,13 +31,14 @@ $_MODULE['<{sendreviewrequest}prestashop>sendreviewrequest_9b3015de76519ac4126b0
 $_MODULE['<{sendreviewrequest}prestashop>sendreviewrequest_a69e35f9e9f418370dbf019c871cc405'] = 'Wählen Sie aus, welche Revws-Aktion für das Krona Modul verwendet werden soll';
 $_MODULE['<{sendreviewrequest}prestashop>sendreviewrequest_f580fe9e5706d2bb634dc973d1aaf74a'] = 'review_created';
 $_MODULE['<{sendreviewrequest}prestashop>sendreviewrequest_3fb1eab339f379f5fb05222478009b75'] = 'review_approved';
-$_MODULE['<{sendreviewrequest}prestashop>sendreviewrequest_f120254f109d626d73ddddeb9cda26e5'] = 'Beim nächsten Vorgang werden %d E-Mail (s) gesendet';
-$_MODULE['<{sendreviewrequest}prestashop>sendreviewrequest_4dd169bc74701e14ae666f19af9b5f5f'] = 'E-Mails zu versenden';
+$_MODULE['<{sendreviewrequest}prestashop>sendreviewrequest_f4f70727dc34561dfde1a3c529b6205c'] = 'Einstellungen';
 $_MODULE['<{sendreviewrequest}prestashop>sendreviewrequest_c9cc8cce247e49bae79f15173ce97354'] = 'Speichern';
+$_MODULE['<{sendreviewrequest}prestashop>sendreviewrequest_4dd169bc74701e14ae666f19af9b5f5f'] = 'E-Mails zu versenden';
+$_MODULE['<{sendreviewrequest}prestashop>sendreviewrequest_f120254f109d626d73ddddeb9cda26e5'] = 'Beim nächsten Vorgang werden %d E-Mail (s) gesendet';
 $_MODULE['<{sendreviewrequest}prestashop>stats_b2d37ae1cedf42ff874289b721860af2'] = 'Protokolle';
 $_MODULE['<{sendreviewrequest}prestashop>stats_3fa3d4c48a662ab0a53aa84c6dd6146e'] = 'Protokolle für die letzten 30 Tage:';
-$_MODULE['<{sendreviewrequest}prestashop>stats_d41455f28486975bfab163b66a473502'] = 'Gesendet = Anzahl der gesendeten E-Mails';
+$_MODULE['<{sendreviewrequest}prestashop>stats_a0071698681b7da543642fe4413f0517'] = 'Datum = Datum der gesendeten e-mails';
+$_MODULE['<{sendreviewrequest}prestashop>stats_d41455f28486975bfab163b66a473502'] = 'Gesendet = Anzahl der gesendeten e-mails';
 $_MODULE['<{sendreviewrequest}prestashop>stats_44749712dbec183e983dcd78a7736c41'] = 'Datum';
-$_MODULE['<{sendreviewrequest}prestashop>stats_6c33c163f04f0f5b477f5e6fbc80d133'] = 'Bewertungsanfrage(n) gesendet';
 $_MODULE['<{sendreviewrequest}prestashop>stats_7f8c0283f16925caed8e632086b81b9c'] = 'Gesendet';
-$_MODULE['<{sendreviewrequest}prestashop>stats_85b6769250887ba6c16099596c75164d'] = 'Zur Zeit keine Statistiken.';
+$_MODULE['<{sendreviewrequest}prestashop>stats_f5d49d90999beb80d572217b920a4299'] = 'Keine Protokoll Informationen zu diesem Zeitpunkt.';
